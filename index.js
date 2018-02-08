39c192a9c38a6c1bcd673e5e147dd07a93beda79
Björn Ricks <bjoern.ricks@gmail.com>
2018-02-08 20:08:54 +0100

    Add draft for login
    Currently this isn't working because gsad is redirecting after the login
    call and the session cookie isn't set yet.

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const program = require('commander');
 const chalk = require('chalk');
+const fetch = require('node-fetch');
+const FormData = require('form-data');
 
 program
   .version('1.0.0')
@@ -9,4 +11,25 @@ program
   .option('-p, --password <password>', 'Login password')
   .parse(process.argv);
 
-  console.log(chalk.blue('Username'), program.username);
+if (program === undefined || program.password === undefined) {
+  program.help();
+  process.exit(1);
+}
+
+console.log(chalk.blue('Username'), program.username);
+console.log(chalk.blue('Password'), program.password);
+
+const form = new FormData();
+form.append('login', program.username);
+form.append('password', program.password);
+form.append('cmd', 'login');
+form.append('no_redirect', '1');
+form.append('text', '/omp?xml=1');
+
+fetch('http://127.0.0.1:9392/omp', {
+  method: 'POST',
+  body: form,
+})
+  .then(res => res.text())
+  .then(text => console.log(chalk.green('Response'), text))
+  .catch(error => console.error(chalk.red('Error'), error));
